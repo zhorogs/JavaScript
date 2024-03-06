d94c1d89e88d9f7570aade4f5dfb6918d5b626c1
zhorogs <zgslavchev@gmail.com>
2024-03-06 23:09:19 +0200

    add imports

---
+++ ReactApps/01-starting-project/src/data.js
@@ -0,0 +1,4 @@
+import componentsImg from './assets/components.png';
+import propsImg from './assets/config.png';
+import jsxImg from './assets/jsx-ui.png';
+import stateImg from './assets/state-mgmt.png';
