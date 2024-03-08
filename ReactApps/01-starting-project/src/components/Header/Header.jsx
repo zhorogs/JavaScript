5fc2c1ecfc7e561b3e84f9176e45a1eadcf99b94
zhorogs <zgslavchev@gmail.com>
2024-03-08 22:41:32 +0200

    move header to its own folder

--- ReactApps/01-starting-project/src/components/Header/Header.jsx
+++ ReactApps/01-starting-project/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import reactImage from '../assets/react-core-concepts.png';
+import reactImage from '../../assets/react-core-concepts.png';
 import "./Header.css";
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
