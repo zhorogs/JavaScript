ac93e175d7117d6fd8c3c8978cabafc64b2af3d1
zhorogs <zgslavchev@gmail.com>
2024-03-10 22:58:22 +0200

    react strict mode on

--- ReactApps/jonasapp/my-react-app/src/index.js
+++ ReactApps/jonasapp/my-react-app/src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDom from "react-dom/client";
 
 function App() {
-    return <h1>Hello React</h1>
+    return <h1>Hello React!</h1>
 };
 
+
+// React V18
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>);
+
+// React before 18;
+// React.render(<App />)
