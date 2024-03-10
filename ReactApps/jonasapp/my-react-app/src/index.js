0f3f6d24f75ba430f96b14e3bcbfab9f4cff7c14
zhorogs <zgslavchev@gmail.com>
2024-03-10 22:55:03 +0200

    hello react

--- ReactApps/jonasapp/my-react-app/src/index.js
+++ ReactApps/jonasapp/my-react-app/src/index.js
@@ -1,3 +1,9 @@
-import React from "React";
+import React from "react";
 import ReactDom from "react-dom/client";
 
+function App() {
+    return <h1>Hello React</h1>
+};
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
