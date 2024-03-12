0859255dce6cb87da1c40da7e0514abe3a3c5d35
zhorogs <zgslavchev@gmail.com>
2024-03-12 23:05:08 +0200

    change h2 to h3 for pizza component

--- ReactApps/jonasapp/my-react-app/src/index.js
+++ ReactApps/jonasapp/my-react-app/src/index.js
@@ -104,7 +104,7 @@ function Pizza() {
     return (
         <div>
             <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-            <h2>Pizza Spinaci</h2>
+            <h3>Pizza Spinaci</h3>
             <p>Tomato, mozarella, spinach, and ricotta cheese.</p>
         </div>
     )
