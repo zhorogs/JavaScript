e5aecfba3294fd152453dcdfadaa68cca4fb12e4
zhorogs <zgslavchev@gmail.com>
2024-03-18 23:58:22 +0200

    add skeleton

---
+++ ReactApps/steps/src/App.js
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p className="message">Hello</p>
+
+      <div className="buttons">
+        <button>Previous</button>
+        <button>Next</button>
+      </div>
+
+    </div>
+  )
+}
