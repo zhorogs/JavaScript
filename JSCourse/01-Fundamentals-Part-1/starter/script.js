b0df675344b467d5a27ac2663f815bb72f6d219c
zhorogs <zgslavchev@gmail.com>
2024-04-07 23:56:06 +0300

    datatypes and typeof

--- JSCourse/01-Fundamentals-Part-1/starter/script.js
+++ JSCourse/01-Fundamentals-Part-1/starter/script.js
@@ -64,3 +64,8 @@
 true;
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Zhoro');
