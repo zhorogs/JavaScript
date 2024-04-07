4124bd022654227e73ccfa684a228e61a635ded7
zhorogs <zgslavchev@gmail.com>
2024-04-07 23:33:42 +0300

    add assignments js file to practice

---
+++ JSCourse/01-Fundamentals-Part-1/assigments.js
@@ -0,0 +1,9 @@
+/* ASSIGNMENT N1 */;
+
+/* Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+Log their values to the console. */
+
+let country = 'Bulgaria';
+let continent = 'Europe';
+let population = '7000000';
