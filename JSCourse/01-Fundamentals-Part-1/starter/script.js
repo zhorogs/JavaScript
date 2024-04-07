ae1537f0ae3e4ec78baff33418abe6c6cbe8a55a
zhorogs <zgslavchev@gmail.com>
2024-04-07 23:22:22 +0300

    refreshing js

--- JSCourse/01-Fundamentals-Part-1/starter/script.js
+++ JSCourse/01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,15 @@
-let js = "amazing";
-if (js === "amazing") alert("js is fun");
+let js = 'amazing';
+if (js === 'amazing') alert('js is fun');
 
 console.log(40 + 8 + 23 - 10);
 
-console.log("Zhoro");
+console.log('Zhoro');
 console.log(23);
 
-let firstName = "Zhoro";
+let firstName = 'Zhoro';
+let first = 'zhoro';
+let first_name_person = 'zhoro_';
+
 //declaring a variable
 console.log(firstName);
 console.log(firstName);
