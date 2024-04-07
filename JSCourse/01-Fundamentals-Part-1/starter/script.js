473c1ed9a227eb6dd76b64d0650870404d7de7b8
zhorogs <zgslavchev@gmail.com>
2024-04-07 23:52:47 +0300

    comment out previous lecture

--- JSCourse/01-Fundamentals-Part-1/starter/script.js
+++ JSCourse/01-Fundamentals-Part-1/starter/script.js
@@ -1,49 +1,66 @@
+
 /* VALUES AND VARIABLES */
 
-let js = 'amazing';
-if (js === 'amazing') alert('js is fun');
+//let js = 'amazing';
+//if (js === 'amazing') alert('js is fun');
 
-console.log(40 + 8 + 23 - 10);
+//console.log(40 + 8 + 23 - 10);
 
-console.log('Zhoro');
-console.log(23);
+//console.log('Zhoro');
+//console.log(23);
 
-let firstName = 'Zhoro';
-let first = 'zhoro';
-let first_name_person = 'zhoro_';
+//let firstName = 'Zhoro';
+//let first = 'zhoro';
+//let first_name_person = 'zhoro_';
 
 //declaring a variable
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+//console.log(firstName);
+//console.log(firstName);
+//console.log(firstName);
 
 /* let 3years = 3 - prohibited, js wont let us; syntax error; invalid or expected token */;
 
 /* let zhor&dudu = 'ZD' prohibited, js wont let us; syntax error; invalid or expected token */;
 
-let zhoro_dudi = 'ZD works';
+//let zhoro_dudi = 'ZD works';
 
 /* let new = 27; cannot use reserver key words, same goes for function, etc */;
 
-let name = 'zhoro';
+//let name = 'zhoro';
 
 /* 'name' keyword can cause problems, but (seems bad practice?) */;
 
-let Person = 'zhoro';
+//let Person = 'zhoro';
 
 /* convention to not start variable names with upper case, variables with upper case naming are *reserved* for constats, like Pi */;
 
 /* variable names should be descriptive */;
 
-let myFirstJob = 'support l1';
-let myCurrentJob = 'support l2'
+//let myFirstJob = 'support l1';
+//let myCurrentJob = 'support l2'
 /* right in the meow meow */;
 
-let job1 = 'support l1';
-let job2 = 'support l2';
+//let job1 = 'support l1';
+//let job2 = 'support l2';
 
-console.log(myFirstJob);
+//console.log(myFirstJob);
 
 /* DATA TYPES */
 
+/* there are 7 primitive data types 
+1. Number - Floating point numbers, used for decimal and integers let age = 23;
+2. String - Sequence of characters, used for text let firstname = 'zhoro;
+3. Boolean - Logical type that can only be true or false, used for taking decisions let fullAge = true;
+4. Undefined - Value taken by a variable taht is not yet defiend ('empty value') let children;
+5. Null - also means 'empty value';
+6. Symbol (ES2015) - Value that is unique and cannot be changed
+7. BigInt (E2020) - Larger integers than the Number type can hold
+*/
 
+/* JS has dynamic typing, we DO NOT have to manually define the data type of the value stored in the variable. Instead, data types are determined automatically */
+
+/* its the value that has a type, not the variable */
+
+true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
