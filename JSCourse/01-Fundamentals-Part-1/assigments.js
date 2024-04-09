027cb2b05cf62f3b0b6eae13a8b66084872ed9f9
zhorogs <zgslavchev@gmail.com>
2024-04-09 23:17:33 +0300

    finish second assignment

--- JSCourse/01-Fundamentals-Part-1/assigments.js
+++ JSCourse/01-Fundamentals-Part-1/assigments.js
@@ -16,3 +16,13 @@ console.log(continent);
 console.log(population);
 */
 
+/* ASSIGNMENT N2 Data Types
+Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+Log the types of isIsland, population, country and language to the console.
+*/;
+
+let isIsland = false;
+let language;
+
+console.log(isIsland, population, country, language);
