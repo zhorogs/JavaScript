1e93f9bb998c9067e2377d1a2b4f8a059948efd3
zhorogs <zgslavchev@gmail.com>
2024-04-13 14:25:03 +0300

    finish first BMI challenge

---
+++ JSCourse/01-Fundamentals-Part-1/starter/challenges.js
@@ -0,0 +1,35 @@
+//FIRST CODING CHALLENGE 
+/* CHALLENGE #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
+
+Your task is to write some code to help them:
+
+Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+
+Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+
+Log the value of BMIMark and BMIJohn to the console.
+
+BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
+
+TEST DATA 1: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+*/
+
+//BMI = mass / (height * height);
+
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark);
+console.log(BMIJohn);
+
+let markHigherBMI = BMIMark > BMIJohn
+console.log(markHigherBMI);
