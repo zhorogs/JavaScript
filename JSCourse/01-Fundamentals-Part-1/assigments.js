f7e820d9f5f22bbb2bbf5a2888eea7a84d13bb41
zhorogs <zgslavchev@gmail.com>
2024-04-30 23:26:37 +0300

    finish assignment n9

--- JSCourse/01-Fundamentals-Part-1/assigments.js
+++ JSCourse/01-Fundamentals-Part-1/assigments.js
@@ -248,5 +248,13 @@ switch (language) {
 
 /*ASSIGNMENT N9 */
 
+/*
+If your country's population is greater than 33 million, use the ternary operator to log a string like this to the console: "Portugal's population is above average". Otherwise, simply log "Portugal's population is below average". Notice how only one word change between these two sentences!
+
+After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original.
+*/
+
+population > 33 ? console.log(`${country}'s population is above average`) : console.log(`${country}'s population is below average`);
+
 
 
