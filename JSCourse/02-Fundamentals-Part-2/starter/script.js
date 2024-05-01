f4a1d42187dc5fc76df1c3e73a43889793d56d40
zhorogs <zgslavchev@gmail.com>
2024-05-01 23:41:55 +0300

    finish testing strict mode

--- JSCourse/02-Fundamentals-Part-2/starter/script.js
+++ JSCourse/02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,15 @@
 'USE STRICT';
 
+//we can use strict for a specific function or specific block of code;
+
+//strict mode forbids us to do certain things and will create visible errors for us in certain situations;
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`i can drive`);
+
+//strict mode introduces a short list of variable names that are reserved, will console log if it happens;
+
+//const if = 23;
