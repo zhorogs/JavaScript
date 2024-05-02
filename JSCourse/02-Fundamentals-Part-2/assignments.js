6c96d6875d6e7d9deb00fe9994d50abfd77c9ff0
zhorogs <zgslavchev@gmail.com>
2024-05-02 23:08:59 +0300

    start assignment n1

---
+++ JSCourse/02-Fundamentals-Part-2/assignments.js
@@ -0,0 +1,8 @@
+//ASSIGNMENT N1
+
+/*
+Write a function called describeCountry which takes three parameters: country, population and capitalCity. Based on this input, the function returns a string with this format: 'Finland has 6 million people and its capital city is Helsinki'.
+
+Call this function 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console.
+*/
+
