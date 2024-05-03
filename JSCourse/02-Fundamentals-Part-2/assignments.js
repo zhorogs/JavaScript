6cbc801cab3727e21e9d9d175081a270730595fb
zhorogs <zgslavchev@gmail.com>
2024-05-03 23:48:37 +0300

    start assignment n2

--- JSCourse/02-Fundamentals-Part-2/assignments.js
+++ JSCourse/02-Fundamentals-Part-2/assignments.js
@@ -6,7 +6,7 @@ Write a function called describeCountry which takes three parameters: country, p
 Call this function 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console.
 */
 
-/* 
+/*
 function describeCountry(country, population, capitalCity) {
     return `${country} has ${population} million people and its capital city is ${capitalCity}`;
 }
@@ -17,3 +17,15 @@ const descFinland = describeCountry('Finland', 6, 'Helsinki');
 
 console.log(descPortugal, descGermany, descFinland);
 */
+
+//ASSIGNMENT N2;
+/*
+The world population is 7900 million people. Create a function declaration called percentageOfWorld1 which receives a population value, and returns the percentage of the world population that the given population represents. For example, China has 1441 million people, so it's about 18.2% of the world population.
+
+To calculate the percentage, divide the given population value by 7900 and then multiply by 100.
+
+Call percentageOfWorld1 for 3 populations of countries of your choice, store the results into variables, and log them to the console.
+
+Create a function expression which does the exact same thing, called percentageOfWolrd2, and also call it with 3 country populations (can be the same populations).
+*/
+
