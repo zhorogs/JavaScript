a5456fc3033afdd45093f1eec18938c8aeffdf30
zhorogs <zgslavchev@gmail.com>
2024-05-04 23:49:15 +0300

    finish assignment n3

--- JSCourse/02-Fundamentals-Part-2/assignments.js
+++ JSCourse/02-Fundamentals-Part-2/assignments.js
@@ -46,3 +46,14 @@ const powexpression = function (population) {
     return population / 7900;
 };
 */
+
+//ASSIGNMENT N3;
+
+//Recreate the last assignment, but this time create an arrow function called percentageOfWorld3.
+
+const percentageOfWorld3 = population => (population / 7900) * 100;
+const percPortugal3 = percentageOfWorld3(10);
+const percChina3 = percentageOfWorld3(1441);
+const percUSA3 = percentageOfWorld3(332);
+
+console.log(percPortugal3, percChina3, percUSA3);
