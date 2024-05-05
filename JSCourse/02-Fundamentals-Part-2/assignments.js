afa378e196f71fc9d7e9d7fec0e75d44e566ae63
zhorogs <zgslavchev@gmail.com>
2024-05-05 23:56:07 +0300

    finish assignment n4

--- JSCourse/02-Fundamentals-Part-2/assignments.js
+++ JSCourse/02-Fundamentals-Part-2/assignments.js
@@ -62,3 +62,30 @@ console.log(percPortugal3, percChina3, percUSA3);
 
 //ASSIGNMENT N4;
 
+/* 
+Create a function called describePopulation.Use the function type you like the most.This function takes in two arguments: country and population, and returns a strings like this: 'China has 1441 million people, which is about 18.2% of the world'.
+
+To calculate the percentage, describePopulation calls the percentageOfWorld1 you created earlier.
+
+Call describePopulation with data for 3 countries of your choice.
+*/
+
+function describePopulation(country, population) {
+
+    const percentofPop = percentageOfWorld3(population);
+    const description = `${country} has ${population} million people, which is about ${percentofPop}% of the world.`
+    return description
+};
+
+function percentageOfWorld3(population) {
+    return (population / 7900) * 100;
+};
+
+const Bulgaria = describePopulation('Bulgaria', 8);
+const Turkey = describePopulation('Turkey', 85);
+const Greece = describePopulation('Greece', 10);
+
+console.log(Bulgaria);
+console.log(Turkey);
+console.log(Greece);
+
