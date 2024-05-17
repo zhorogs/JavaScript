9517db7d3d0322c4b160e9cdfc41aeda1b27d63c
zhorogs <zgslavchev@gmail.com>
2024-05-17 23:41:33 +0300

    finish assignmet n5

--- JSCourse/02-Fundamentals-Part-2/assignments.js
+++ JSCourse/02-Fundamentals-Part-2/assignments.js
@@ -100,3 +100,16 @@ Log to the console whether the array has 4 elements or not(true or false).
 
 Create an array called percentages containing the percentages of the world population for these 4 population values.Use the function percentageOfWorld1 that you created earlier to compute the 4 percentage values.
 */
+
+const populations = [10, 1441, 332, 83];
+
+console.log(populations.length === 4);
+
+const percentages = [
+    percentageOfWorld1(populations[0]),
+    percentageOfWorld1(populations[1]),
+    percentageOfWorld1(populations[2]),
+    percentageOfWorld1(populations[3])
+];
+
+console.log(percentages);
