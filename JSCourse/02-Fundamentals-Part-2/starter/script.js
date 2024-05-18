0d5b41cc9ce7eb5de9685f1a20a1e3f6bc212d63
zhorogs <zgslavchev@gmail.com>
2024-05-19 00:05:49 +0300

    finish array basics

--- JSCourse/02-Fundamentals-Part-2/starter/script.js
+++ JSCourse/02-Fundamentals-Part-2/starter/script.js
@@ -250,4 +250,13 @@ const years = [1990, 1967, 2002, 2010, 2018];
 
 //cannot do
 //calcAge(years);
-//returns NaN
+//returns NaN;
+
+const age1 = calcAge(years[0])
+const age2 = calcAge(years[1])
+const age3 = calcAge(years[years.length - 1])
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+// we can place function calls into arrays, because the produce a value, an expression;
+console.log(ages);
