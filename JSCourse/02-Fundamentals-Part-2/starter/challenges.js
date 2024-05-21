a1718bd109f60eaf8b9ea84b4000d5209a8fb7c3
zhorogs <zgslavchev@gmail.com>
2024-05-21 23:38:49 +0300

    finish challenge 2

--- JSCourse/02-Fundamentals-Part-2/starter/challenges.js
+++ JSCourse/02-Fundamentals-Part-2/starter/challenges.js
@@ -74,3 +74,15 @@ BONUS: Create an array totals containing the total values, so the bill + tip.
 
 TEST DATA: 125, 555, and 44.
 */
+
+const calcTip = (bill) => {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15
+    } else return bill * 0.20
+}
+
+const bills = [125, 555, 44]
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
