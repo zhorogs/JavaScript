a0e58d915fe35c54a61804acb030c99528bcab6b
zhorogs <zgslavchev@gmail.com>
2024-05-21 23:53:40 +0300

    assignment n6

--- JSCourse/02-Fundamentals-Part-2/assignments.js
+++ JSCourse/02-Fundamentals-Part-2/assignments.js
@@ -93,7 +93,7 @@ console.log(Greece);
 
 //ASSIGNMENT N5;
 
-/* 
+/*
 Create an array containing 4 population values of 4 countries of your choice.You may use the values you have been using previously.Store this array into a variable called populations.
 
 Log to the console whether the array has 4 elements or not(true or false).
@@ -101,7 +101,7 @@ Log to the console whether the array has 4 elements or not(true or false).
 Create an array called percentages containing the percentages of the world population for these 4 population values.Use the function percentageOfWorld1 that you created earlier to compute the 4 percentage values.
 */
 
-/* 
+/*
 const populations = [10, 1441, 332, 83];
 
 console.log(populations.length === 4);
@@ -114,4 +114,19 @@ const percentages = [
 ];
 
 console.log(percentages);
+*/
+
+//ASSIGNMENT N6;
+
+/*
+Basic Array Operations (Methods)﻿
+Create an array containing all the neighbouring countries of a country of your choice. Choose a country which has at least 2 or 3 neighbours. Store the array into a variable called neighbours.
+
+At some point, a new country called 'Utopia' is created in the neighbourhood of your selected country, so add it to the end of the neighbours array.
+
+Unfortunately, after some time the new country is dissolved, so remove it from the end of the array.
+
+If the neighbours array does not include the country 'Germany', log to the console: 'Probably not a central european country :D'.
+
+Change the name of one of your neighbouring countries. To do that, find the index of the country in the neighbours array, and then use that index to change the array at that index position. For example, you can search for 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
 */
