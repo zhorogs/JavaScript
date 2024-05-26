f1952dfcbcc74e3d5d200f2f45877f3b3b3d9595
zhorogs <zgslavchev@gmail.com>
2024-05-26 23:50:24 +0300

    fininsh object assignment

--- JSCourse/02-Fundamentals-Part-2/assignments.js
+++ JSCourse/02-Fundamentals-Part-2/assignments.js
@@ -155,4 +155,13 @@ Introduction to Objects﻿
 Create an object called myCountry for a country of your choice, containing properties country, capital, language, population and neighbours (an array like we used in previous assignments).
 */
 
+/* 
+const myCountry = {
+    country: 'Bulgaria',
+    capital: 'Sofia',
+    language: 'Bulgarian',
+    population: 8,
+    neighbours: [Turkey, Greece, Serbia, Romania]
+};
+*/
 
