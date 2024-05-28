497afde119da0b51844b9f884a657578c463fc29
zhorogs <zgslavchev@gmail.com>
2024-05-28 23:48:44 +0300

    finish assignment

--- JSCourse/02-Fundamentals-Part-2/assignments.js
+++ JSCourse/02-Fundamentals-Part-2/assignments.js
@@ -173,3 +173,15 @@ Using the object from the previous assignment, log a string like this to the con
 
 Increase the country's population by two million using dot notation, and then decrease it by two million using bracket notation.
 */
+
+/*
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
+);
+
+myCountry.population += 2;
+console.log(myCountry.population);
+
+myCountry['population'] -= 2;
+console.log(myCountry.population);
+*/
