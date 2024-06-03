1bd6d69ec5e516e8709188a00d0314d5080e4735
zhorogs <zgslavchev@gmail.com>
2024-06-03 23:48:36 +0300

    object methods assignment

--- JSCourse/02-Fundamentals-Part-2/starter/script.js
+++ JSCourse/02-Fundamentals-Part-2/starter/script.js
@@ -397,3 +397,10 @@ console.log(`${zhoroObject.firstName} has ${zhoroObject.friends.length} friends,
 /* TEST COMMIT */
 
 /* OBJECT METHDS */
+
+/*
+Add a method called describe to the myCountry object. This method will log a string to the console, similar to the string logged in the previous assignment, but this time using the 'this' keyword.
+
+Call the describe method.
+
+Add a method called checkIsland to the myCountry object. This method will set a new property on the object, called isIsland. isIsland will be true if there are no neighbouring countries, and false if there are. Use the ternary operator to set the property.&*/
