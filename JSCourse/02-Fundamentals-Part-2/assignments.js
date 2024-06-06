643e989c523cbffa5af0ab05282a31dcbe8c3597
zhorogs <zgslavchev@gmail.com>
2024-06-06 23:22:28 +0300

    looping arrays

--- JSCourse/02-Fundamentals-Part-2/assignments.js
+++ JSCourse/02-Fundamentals-Part-2/assignments.js
@@ -198,3 +198,11 @@ Add a method called checkIsland to the myCountry object. This method will set a 
 */
 
 /* Looping Arrays, Breaking and Continuing */
+
+/* 
+Let's bring back the populations array from a previous assignment.
+
+Use a for loop to compute an array called percentages2 containing the percentages of the world population for the 4 population values. Use the function percentageWOrld1 that you created earlier.
+
+Confirm that percentages2 contains exactly the same values as the percentages array that we created manually in the previous assignment, and reflect on how much better this solution is.
+*/
