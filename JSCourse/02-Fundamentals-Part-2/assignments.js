05771cf88df8344725f2e7a04b70a0f318087a55
zhorogs <zgslavchev@gmail.com>
2024-06-07 23:23:23 +0300

    looping backwards and loops in loops

--- JSCourse/02-Fundamentals-Part-2/assignments.js
+++ JSCourse/02-Fundamentals-Part-2/assignments.js
@@ -208,3 +208,12 @@ Confirm that percentages2 contains exactly the same values as the percentages ar
 */
 
 /* Looping Backwards and Loops in Loops */
+
+/* 
+
+Store this array of arrays into a variable called listOfNeighbours:
+
+[['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+Log only the neighbouring countries to the console, one by one, not the entire arrays. Log a string like 'Neighbour: Canada' for each country.
+
+You will need a loop inside a loop for this. This is actually a bit tricky, so don't worry if it's too difficult for you! But you can still try to figure this out anyway 😉*/
